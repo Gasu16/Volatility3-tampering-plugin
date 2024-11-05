6864d461079679f09dbfc8cc4953176b2130f504
Rosario Matteo GRAMMATICO <rmgrammatico@it.iliad.com>
2024-11-06 00:51:38 +0100

    trying to fix something

--- tampering.py
+++ tampering.py
@@ -6,7 +6,7 @@ import codecs
 import contextlib
 
 from typing import Any, Generator, List, Tuple
-from volatility3.framework import objects, constants, exceptions, interfaces, renderers
+from volatility3.framework import automagic, objects, constants, exceptions, interfaces, renderers
 from volatility3.framework.configuration import requirements
 from volatility3.framework.layers.physical import BufferDataLayer
 from volatility3.framework.symbols import intermed
@@ -25,7 +25,7 @@ class Tampering(interfaces.plugins.PluginInterface, timeliner.TimeLinerInterface
     _version = (1, 1, 0)
 
     @classmethod
-    def get_requirements(cls):
+    def get_requirements(cls) -> List[interfaces.configuration.RequirementInterface]:
         return [requirements.ModuleRequirement(name = 'kernel', description = 'Windows kernel', architectures = ["Intel32", "Intel64"]),
                     requirements.PluginRequirement(
                         name = 'printkey',
@@ -33,17 +33,41 @@ class Tampering(interfaces.plugins.PluginInterface, timeliner.TimeLinerInterface
                         version = (1, 1, 0)
                     )
                 ]
+    def print_tampering_keys(cls): # Static Keys for tampering took from Windows registry
+        return [
+            printkey.Printkey._printkey_iterator(hive="Microsoft\\Windows Defender\\Features") # Boh da capire
+        ]
     def run(self):
+        automagics = automagic.choose_automagic(self.automagics, plugin_class)
+        plugin = plugins.construct_plugin(
+                    self.context,
+                    automagics,
+                    plugin_class,
+                    self.config_path,
+                    self._progress_callback,
+                    self.open,
+        )
         return renderers.TreeGrid(
             columns = [
                 # name_column = value_column
+                ("Last Write Time", datetime.datetime),
+                ("Hive Offset", format_hints.Hex),
+                ("Type", str),
+                ("Key", str),
+                ("Name", str),
+                ("Data", format_hints.MultiTypeData),
+                ("Volatile", bool),
             ],
-            generator = self.generator(
+            generator = self._generator(
                 # da decidere see fare _printkey_iterator o _registry_walker, guardare meglio printkey.py
-                printkey.Printkey._printkey_iterator(
-                    self.context,
-                    kernel.layer_name,
-                    kernel.symbol_table_name
-                ),
+                #self.context,
+                #kernel.layer_name,
+                #kernel.symbol_table_name
+                #printkey.Printkey._printkey_iterator("Microsoft\Windows Defender\Features")
+                #printkey.Printkey._registry_walker(
+                #    self.context,
+                #    kernel.layer_name,
+                #    kernel.symbol_table_name
+                #),
             ),
         )
