8e5629d85a61ad4f89dbcd50c8484c7080679c5a
Rosario Matteo GRAMMATICO <rmgrammatico@it.iliad.com>
2024-12-09 12:57:35 +0100

    printkey style

--- tampering.py
+++ tampering.py
@@ -8,21 +8,18 @@ import winreg
 import win32evtlog
 import xml.etree.ElementTree as ET
 #from winevt import EventLog
+import pdb
 
 from typing import Any, Generator, List, Tuple, Sequence, Iterable
 from volatility3.framework import automagic, objects, constants, exceptions, interfaces, renderers, plugins
 from volatility3.framework.configuration import requirements
-#from volatility3.framework.interfaces import plugins
-#from volatility3.framework.layers import registry
 from volatility3.framework.layers.physical import BufferDataLayer
-#from volatility3.framework.layers.registry import RegistryHive
+from volatility3.framework.layers.registry import RegistryHive, RegistryFormatException
 from volatility3.framework.symbols import intermed
-#from volatility3.plugins.windows.registry import hivelist
 from volatility3.framework.renderers import TreeGrid, conversion, format_hints
-#from volatility3.plugins import timeliner
-#from volatility3.plugins.windows.registry import *
-#from volatility3.plugins.windows.registry import printkey
-#from volatility3.framework.layers.registry import RegistryHive, RegistryFormatException
+from volatility3.framework.symbols.windows.extensions.registry import RegValueTypes
+from volatility3.plugins.windows.registry import hivelist
+
 
 vollog = logging.getLogger(__name__)
 
@@ -81,101 +78,231 @@ tampering_events_id = [
 class Tampering(interfaces.plugins.PluginInterface):
     _required_framework_version = (2, 0, 0)
     _version = (1, 1, 0)
-
-#    @classmethod
-#    def get_requirements(cls) -> List[interfaces.configuration.RequirementInterface]:
-#        return [requirements.ModuleRequirement(name = 'kernel', description = 'Windows kernel', architectures = ["Intel32", "Intel64"]),
-#                    requirements.PluginRequirement(
-#                        name = 'hivelist',
-#                        plugin = hivelist.HiveList,
-#                        version = (1, 0, 0)
-#                    )
-#                ]
-    @classmethod
-    def get_tampering_key(cls, root_hive, key, value):
-        aReg = winreg.ConnectRegistry(None, root_hive)
-        aKey = winreg.OpenKeyEx(aReg, key)
-        sValue = winreg.QueryValueEx(aKey, value)
-        eData = winreg.EnumValue(aKey, 0)
-        l = []
-        l.append(aReg)
-        l.append(aKey)
-        l.append(value)
-        l.append(sValue)
-        return l
     
     @classmethod
-    def detect_tampering_attempts(cls, event_file):
-        # https://learn.microsoft.com/en-us/defender-endpoint/troubleshoot-microsoft-defender-antivirus
-        # Event Viewer > Applications and Services Logs > Microsoft > Windows > Windows Defender > Operational
-        # detect for events: 5004, 5007, 5008, 5010, 5012, 5013, 5100, 5101
-        # https://medium.com/csis-techblog/silencing-microsoft-defender-for-endpoint-using-firewall-rules-3839a8bf8d18
-        # Event Viewer > Applications and Services Logs > Microsoft > Windows > SENSE > Operational
-        # detect for events: 5
-        
-        query_handle = win32evtlog.EvtQuery(event_file, win32evtlog.EvtQueryFilePath)        
-        print(query_handle)
-        read_count = 0
-        n = 100
-        while True:
-            # read 100 records
-            events = win32evtlog.EvtNext(query_handle, n)
-            read_count += len(events)
-            # if there is no record break the loop
-            if len(events) == 0:
-                break
-            for event in events:
-                xml_content = win32evtlog.EvtRender(event, win32evtlog.EvtRenderEventXml)
-                # print(xml_content)
-
-                # parse xml content
-                xml = ET.fromstring(xml_content)
-                # xml namespace, root element has a xmlns definition, so we have to use the namespace
-                ns = '{http://schemas.microsoft.com/win/2004/08/events/event}'
-                event_id = xml.find(f'.//{ns}EventID').text
-                level = xml.find(f'.//{ns}Level').text
-                channel = xml.find(f'.//{ns}Channel').text
-                execution = xml.find(f'.//{ns}Execution')
-                process_id = execution.get('ProcessID')
-                thread_id = execution.get('ThreadID')
-                time_created = xml.find(f'.//{ns}TimeCreated').get('SystemTime')
-                if event_id in tampering_events_id:
-                    print(f'Time: {time_created}, Level: {level} Event Id: {event_id}, Channel: {channel}, Process Id: {process_id}, Thread Id: {thread_id}')
-                user_data = xml.find(f'.//{ns}UserData')
-                # user_data has possible any data
-        print(f'Read {read_count} records')
-        return
-        
-        
-    def run(self):
-        #kernel = self.context.modules[self.config['kernel']]
-        #automagics = automagic.choose_automagic(automagic.available(self._context), hivelist.HiveList)
-        #plugin = plugins.construct_plugin(self.context, automagics, hivelist.HiveList, self.config_path, self._progress_callback, self.open)
-        #root_hive = winreg.HKEY_LOCAL_MACHINE
-        
-        return renderers.TreeGrid([
-                                   ("Root Hive", str),
-                                   ("Key", str),
-                                   ("Data Name", str),
-                                   ("Value", str)],
-                                   self._generator(),
-                                   )
-    def _generator(self):
-        root_hive = winreg.HKEY_LOCAL_MACHINE
-        
-        for _keys in tampering_keys:
-            for _values in tampering_values:
+    def get_requirements(cls) -> List[interfaces.configuration.RequirementInterface]:
+        from volatility3.plugins.windows.registry import hivelist
+        return [requirements.ModuleRequirement(name = 'kernel', description = 'Windows kernel', architectures = ["Intel32", "Intel64"]),
+                    requirements.PluginRequirement(
+                        name = 'hivelist',
+                        plugin = hivelist.HiveList,
+                        version = (1, 0, 0)
+                    )
+                ]
+    @classmethod
+    def get_tampering_key(cls, root_hive: RegistryHive, node_path: Sequence[objects.StructType] = None):
+        node_path = [root_hive.get_node(root_hive.root_cell_offset)]
+        print(node_path)
+        return node_path
+    
+    @classmethod
+    def get_keys(cls, hive: RegistryHive, node_path: Sequence[objects.StructType] = None, recurse: bool = False)-> Generator[Tuple[int, Tuple], None, None]:
+        if not node_path:
+            node_path = [hive.get_node(hive.root_cell_offset)]
+        if not isinstance(node_path, list) or len(node_path) < 1:
+            vollog.warning("Hive walker was not passed a valid node_path (or None)")
+            return None
+        node = node_path[-1]
+        key_path_items = [hive] + node_path[1:]
+        key_path = "\\".join([k.get_name() for k in key_path_items])
+        if node.vol.type_name.endswith(constants.BANG + "_CELL_DATA"):
+            raise RegistryFormatException(
+                hive.name, "Encountered _CELL_DATA instead of _CM_KEY_NODE"
+            )
+        last_write_time = conversion.wintime_to_datetime(node.LastWriteTime.QuadPart)
+        for key_node in node.get_subkeys():
+            result = (
+                len(node_path),
+                True,
+                last_write_time,
+                key_path,
+                key_node.get_volatile(),
+                key_node,
+            )
+            yield result
+            
+            if recurse:
+                if key_node.vol.offset not in [x.vol.offset for x in node_path]:
+                    try:
+                        key_node.get_name()
+                    except exceptions.InvalidAddressException as excp:
+                        vollog.debug(excp)
+                        continue
+                        
+                    yield from cls.key_iterator(
+                        hive, node_path + [key_node], recurse=recurse
+                    )
+        for value_node in node.get_values():
+            result = (
+                len(node_path),
+                False,
+                last_write_time,
+                key_path,
+                node.get_volatile(),
+                value_node,
+            )
+            yield result
+            
+    def _printkey(cls, hive: RegistryHive, node_path: Sequence[objects.StructType] = None, recurse: bool = False):
+        for(
+            depth,
+            is_key,
+            last_write_time,
+            key_path,
+            volatile,
+            node,
+        ) in cls.get_keys(hive, node_path, recurse):
+            if is_key:
                 try:
-                    args_ = self.get_tampering_key(root_hive, _keys, _values)
-                    data_ = args_[2] # data value name
-                    value_ = args_[3] # actual type value
-                    #yield (0, (str(root_hive), str(_keys), str(data_), str(value_[0])))
-                except FileNotFoundError:
-                    continue
-                yield (0, (str(root_hive), str(_keys), str(data_), str(value_[0])))
-        for _events in tampering_events_files:
+                    key_node_name = node.get_name()
+                except (
+                    exceptions.InvalidAddressException,
+                    RegistryFormatException,
+                ) as excp:
+                    vollog.debug(excp)
+                    key_node_name = renderers.UnreadableValue()
+                    
+                last_write_time = conversion.wintime_to_datetime(node.LastWriteTime.QuadPart)
+                yield (
+                    depth,
+                    (
+                        last_write_time,
+                        renderers.format_hints.Hex(hive.hive_offset),
+                        "Key",
+                        key_path,
+                        key_node_name,
+                        renderers.NotApplicableValue(),
+                        volatile,
+                    ),
+                )
+            else:
+                try:
+                    value_node_name = node.get_name() or "(Default)"
+                except (
+                    exceptions.InvalidAddressException,
+                    RegistryFormatException,
+                ) as excp:
+                    vollog.debug(excp)
+                    value_node_name = renderers.UnreadableValue()
+                
+                try:
+                    value_type = RegValueTypes(node.Type).name
+                except (
+                    exceptions.InvalidAddressException,
+                    RegistryFormatException,
+                ) as excp:
+                    vollog.debug(excp)
+                    value_type = renderers.UnreadableValue()
+                
+                if isinstance(value_type, renderers.UnreadableValue):
+                    vollog.debug("Couldn't read registry value type, so data is unreadable")
+                    value_data: Union[interfaces.renderers.BaseAbsentValue, bytes] = (renderers.UnreadableValue())
+                else:
+                    try:
+                        value_data = node.decode_data()
+                        
+                        if isinstance(value_data, int):
+                            value_data = format_hints.MultiTypeData(
+                                value_data, encoding="utf-8"
+                            )
+                        elif RegValueTypes(node.Type) == RegValueTypes.REG_BINARY:
+                            value_data = format_hints.MultiTypeData(
+                                value_data, show_hex=True
+                            )
+                        elif RegValueTypes(node.Type) == RegValueTypes.REG_MULTI_SZ:
+                            value_data = format_hints.MultiTypeData(
+                                value_data, encoding="utf-16-le", split_nulls=True
+                            )
+                        else:
+                            value_data = format_hints.MultiTypeData(
+                                value_data, encoding="utf-16-le"
+                            )
+                    except (
+                        ValueError,
+                        exceptions.InvalidAddressException,
+                        RegistryFormatException,
+                    ) as excp:
+                        vollog.debug(excp)
+                        value_data = renderers.UnreadableValue()
+                result = (
+                    depth,
+                    (
+                        last_write_time,
+                        renderers.format_hints.Hex(hive.hive_offset),
+                        value_type,
+                        key_path,
+                        value_node_name,
+                        value_data,
+                        volatile,
+                    ),
+                )
+                yield result
+
+    def run(self):
+        offset = self.config.get("offset", None)
+        kernel = self.context.modules[self.config["kernel"]]
+        
+        return renderers.TreeGrid(
+            [
+                ("Last Write Time", datetime.datetime),
+                ("Hive Offset", format_hints.Hex),
+                ("Type", str),
+                ("Key", str),
+                ("Name", str),
+                ("Data", format_hints.MultiTypeData),
+                ("Volatile", bool),
+            ],
+            self._generator(
+                kernel.layer_name,
+                kernel.symbol_table_name,
+                hive_offsets=None if offset is None else [offset],
+                key=self.config.get("key", None),
+                recurse=self.config.get("recurse", None),
+            ),
+        )
+
+    def _generator(self, layer_name: str, symbol_table: str, hive_offsets: List[int] = None, key: str = None, recurse: bool = False,):
+        for hive in hivelist.HiveList.list_hives(
+            self.context,
+            self.config_path,
+            layer_name = layer_name,
+            symbol_table = symbol_table,
+            hive_offsets = hive_offsets
+        ):
             try:
-                self.detect_tampering_attempts(_events)
-            except Exception:
-                print("\nYou need to run this plugin as Administrator if you want to get the event logs info")
-                continue
+                if key is not None:
+                    node_path = hive.get_key(key, return_list=True)
+                else:
+                    node_path = [hive.get_node(hive.root_cell_offset)]
+                for x, y in self._printkey(hive, node_path, recurse=recurse):
+                    yield (x - len(node_path), y)
+            except (
+                exceptions.InvalidAddressException,
+                KeyError,
+                RegistryFormatException,
+            ) as excp:
+                if isinstance(excp, KeyError):
+                    vollog.debug(
+                        f"key '{key}' not found in Hive at offset {hex(hive.hive_offset)}."
+                    )
+                elif isinstance(excp, RegistryFormatException):
+                    vollog.debug(excp)
+                elif isinstance(excp, exceptions.InvalidAddressException):
+                    vollog.debug(
+                        f"Inalid address identified in Hive: {hex(excp.invalid_address)}"
+                    )
+                result = (
+                    0,
+                    (
+                        renderers.UnreadableValue(),
+                        format_hints.Hex(hive.hive_offset),
+                        "Key",
+                        f"{hive.get_name()}\\" + (key or ""),
+                        renderers.UnreadableValue(),
+                        renderers.UnreadableValue(),
+                        renderers.UnreadableValue(),
+                    ),
+                )
+                yield result
+        
+        return True
